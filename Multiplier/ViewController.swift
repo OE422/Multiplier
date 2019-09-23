5815613d4a9fa2de83586d35c26ede19f1348fd1
OE422 <omareldoronki@gmail.com>
2019-09-23 14:20:17 -0400

    not done

--- Multiplier/ViewController.swift
+++ Multiplier/ViewController.swift
@@ -8,11 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    var textField1 = UITextField()
+    var textField2 = UITextField()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.becomeFirstResponder()
+        
+        textFieldSetup(txtFld: textField1, _arg: 100)
+        
+    }
+    func textFieldSetup (arg txtFld: UITextField, arg yCoord: Int)
+    {
+        
     }
 
 
