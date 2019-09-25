8205c3a64db096f9d812ab1a017be64f5e9fcbf9
OE422 <omareldoronki@gmail.com>
2019-09-25 14:18:08 -0400

    added operator buttons, still have to add those buttons to the stack view and give them functionality

--- Multiplier/ViewController.swift
+++ Multiplier/ViewController.swift
@@ -12,14 +12,28 @@ class ViewController: UIViewController, UITextFieldDelegate {
 
     var textField1 = UITextField()
     var textField2 = UITextField()
+    var add = UIButton()
+    var subtract = UIButton()
+    var multiply = UIButton()
+    var divide = UIButton()
+    var BowserJr = UIImageView()
     var productLabel = UILabel()
     var stackView = UIStackView()
     override func viewDidLoad() {
         super.viewDidLoad()
         self.becomeFirstResponder()
         
+        view.backgroundColor = UIColor.lightGray
         textFieldSetup(arg: textField1)
         textFieldSetup(arg: textField2)
+        
+        BowserJr.image = UIImage(named: "BowserJr")
+//        BowserJr.frame = CGRect(x: BowserJr.frame.origin.x, y: BowserJr.frame.origin.y, width: 100, height: 142)
+         BowserJr.frame = CGRect.zero
+        BowserJr.isHidden = true
+        BowserJr.contentMode = UIView.ContentMode.scaleAspectFit
+        self.view.addSubview(BowserJr)
+        BowserJr.translatesAutoresizingMaskIntoConstraints = false
         
         stackView.axis = .vertical
         stackView.distribution = UIStackView.Distribution.equalSpacing
@@ -28,12 +42,16 @@ class ViewController: UIViewController, UITextFieldDelegate {
         stackView.addArrangedSubview(textField1)
         stackView.addArrangedSubview(textField2)
         stackView.addArrangedSubview(productLabel)
+//        stackView.addArrangedSubview(BowserJr)
         self.view.addSubview(stackView)
         stackView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: (UIScreen.main.bounds.width/2)-((stackView.frame.maxX-stackView.frame.minX)/2), y: stackView.frame.origin.y).isActive = true
-        stackView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 50).isActive = true
+        stackView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 50).isActive = true
+        stackView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -50).isActive = true
         stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
-        stackView.heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.height/4).isActive = true
+        
+        BowserJr.topAnchor.constraint(equalTo: stackView.bottomAnchor, constant: -270).isActive = true
+        BowserJr.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 40).isActive = true
+        BowserJr.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -40).isActive = true
     }
     
     func textFieldSetup (arg txtFld: UITextField)
@@ -56,9 +74,32 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
-
-        productLabel.text = String(Int(textField1.text ?? "1")! * Int(textField2.text ?? "1")!)
+        
+        
+        
+        
+        if(checkIfInt(arg: (Double(textField1.text ?? "1.0")!) * (Double(textField2.text ?? "1.0")!)))
+        {
+            productLabel.text = String(Int((Double(textField1.text ?? "1.0")!)*(Double(textField2.text ?? "1.0")!)))
+            if (Int((Double(textField1.text ?? "1.0")!)*(Double(textField2.text ?? "1.0")!)) == 64)
+            {
+                BowserJr.isHidden = false
+            }
+        }
+        else
+        {
+            productLabel.text = String((Double(textField1.text ?? "1.0")!)*(Double(textField2.text ?? "1.0")!))
+        }
         return true
+    }
+    func checkIfInt (arg x: Double) -> Bool
+    {
+        let isInteger = floor(x)
+        if  (isInteger == x)
+        {
+            return true
+        }
+        return false
     }
 
 
